90a2df531a2a530de0ce9685a51ca334d72db764
jinouwuque <ee2win@gmail.com>
2016-08-07 20:21:16 -0400

    add home entry zhuangbiliqi

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,39 @@
 import { Router, Route, IndexRoute, Link } from 'react-router';
 import React, { Component, PropTypes } from 'react';
-import { Timeline } from 'antd';
+import { Row, Col } from 'antd';
+import QueueAnim from 'rc-queue-anim';
+import styles from './Home.less';
+import Banner1 from './assets/banner1.png';
 
 const Home = () => {
     return (
-        <Timeline pending={<a href="#">查看更多</a>}>
-            <Timeline.Item>创建服务现场 2015-09-01</Timeline.Item>
-            <Timeline.Item>初步排除网络异常 2015-09-01</Timeline.Item>
-            <Timeline.Item>技术测试异常 2015-09-01</Timeline.Item>
-        </Timeline>
+    <Row type="flex" justify="start">
+        <Col span={4}></Col>
+        <Col span={8}>
+            <img src={Banner1} className={styles.banner}/></Col>
+        <Col span={8}>
+            <div className={styles.textContainer}>
+                <QueueAnim delay={500} style={{ height: 150 }}>
+                    <div key="a">
+                        <h1>Next generation zhuangbi liqi</h1>
+                        <br/>
 
+                    </div>
+                    <div key="b">
+                        <h1>Your own visualization notebook in your pocket</h1>
+                        <br/>
+
+                    </div>
+                    <div key="c">
+                        <h1>Based by most zhuangbi tech</h1>
+                        <br/>
+                    </div>
+                </QueueAnim>
+            </div>
+
+        </Col>
+        <Col span={4}></Col>
+    </Row>
     );
 };
 Home.propTypes = {
