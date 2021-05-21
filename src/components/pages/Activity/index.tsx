7fcfeea2efac2abd5a3d6bcb95a61d6523cd504b
im6 <ee2win@gmail.com>
2021-05-21 19:19:56 -0400

    hide filter

--- src/components/pages/Activity/index.tsx
+++ src/components/pages/Activity/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import Html from "../../layout/Html";
-import Nav from "../../partials/Nav";
+//import Nav from "../../partials/Nav";
 import EventTable from "../../partials/EventTable";
 
 interface ActivityProps {
@@ -23,7 +23,7 @@ const Activity: FC<ActivityProps> = ({
       </a>
     </div>
     <div className="activity-layout">
-      <Nav allData={activityDefinition} selected={selectedActivity} />
+      {/* <Nav allData={activityDefinition} selected={selectedActivity} /> */}
       <EventTable data={eventList} activityDef={activityDefinition} />
     </div>
   </Html>
